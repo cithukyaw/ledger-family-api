5164015be7fc33ad4d35c7a8c9cf7f9d4e161168
Sithu <cithukyaw@gmail.com>
2024-07-30 23:23:37 +0630

    Type annotate request body and response body

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,13 +1,29 @@
 import {Request, Response} from 'express';
+import { CreateUserDto } from '../dtos/CreateUser.dto';
+import { User } from '../types/response';
 
 export const getUsers = (req: Request, res: Response) => {
   return res.send([]);
 }
 
-export const getUser = (req: Request, res: Response) => {
-  return res.send({});
+export const getUser = (req: Request<{id: number}>, res: Response) => {
+  const userId = req.params.id;
+  console.log(userId);
+
+  return res.send({
+    id: userId
+  });
 }
 
-export const createUser = (req: Request, res: Response) => {
-  return res.send({});
+export const createUser = (req: Request<{}, {}, CreateUserDto>, res: Response<User>) => {
+  // TODO: create user
+  console.log(req.body)
+
+  const createdUser: User = {
+    id: 1,
+    username: "john.doe",
+    email: "john.doe@example.com"
+  };
+
+  return res.status(201).send(createdUser);
 }
