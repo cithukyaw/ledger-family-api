55e6c908271449f76e01618f348ad7942e1e59e8
Sithu <cithukyaw@gmail.com>
2024-08-10 23:04:58 +0630

    Update seeding for categories

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,18 +4,50 @@ import {ROLE} from "../src/lib/constants";
 
 const prisma = new PrismaClient();
 
-async function seed() {
+const users = async () => {
   const salt = genSaltSync(10)
 
-  await prisma.user.create({
-    data: {
+  await prisma.user.upsert({
+    where: { email: 'admin@localhost.com' },
+    update: {},
+    create: {
       name: 'Administrator',
       email: 'admin@localhost.com',
       password: hashSync('password', salt),
       role: ROLE.ADMIN,
       active: true
     }
+  });
+}
+
+const categories = async () => {
+  const list = [
+    'General',
+    'Shopping',
+    'Purchase',
+    'Bill',
+    'Pharmacy',
+    'Travel',
+    'Health',
+    'Mobile Top-up',
+    'Charity',
+    'Family Eat-out'
+  ];
+
+  list.map(async (catName) => {
+    await prisma.category.upsert({
+      where: { name: catName },
+      update: {},
+      create: {
+        name: catName
+      }
+    });
   })
+}
+
+const seed = async () => {
+  await users();
+  await categories();
 }
 
 seed()
